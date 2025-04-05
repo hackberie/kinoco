f186424d25334f5b7e924a3bd0858358c3c006bc
Masanori ENOKI <enoki@ricotta.local>
2025-04-05 14:35:24 +0900

    Commit at 2025-04-05 14:35:24

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="kinoco",  # プロジェクトの名前
     version="25.04.01",   # バージョン
     packages=find_packages(),  # すべてのパッケージを自動検出
+    include_package_data=True,
     install_requires=[
         "numpy",
         "pandas",
