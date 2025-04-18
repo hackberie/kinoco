0f3ad6c749e034d18d5b089786c2147a2eaef7bb
Masanori ENOKI <enoki@ricotta.local>
2025-04-18 13:40:03 +0900

    Commit at 2025-04-18 13:40:03

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="kinoco",  # プロジェクトの名前
-    version="25.04.01",   # バージョン
+    version="25.04.18",   # バージョン
     packages=find_packages(),  # すべてのパッケージを自動検出
     include_package_data=True,
     install_requires=[
         "numpy",
         "pandas",
         "matplotlib",
-        "seaborn"
+        "seaborn",
+        "adjustText"
     ],  # 依存パッケージ
 )
 
